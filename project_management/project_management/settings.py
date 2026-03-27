ed9143d1815f53ed376a52184fe01d13c7c712ec
Vishal Kannaujiya <vishal.kannaujiya@maviontech.com>
2026-03-27 10:23:42 +0530

    add on middle in settings.py

--- project_management/project_management/settings.py
+++ project_management/project_management/settings.py
@@ -75,6 +75,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'core.middleware.SessionTimeoutMiddleware',
     'core.middleware.TenantMiddleware',
 ]
 
@@ -286,3 +287,8 @@ LOGGING = {
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Session timeout: 30 minutes of inactivity
+SESSION_COOKIE_AGE = 1800
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+SESSION_SAVE_EVERY_REQUEST = True
